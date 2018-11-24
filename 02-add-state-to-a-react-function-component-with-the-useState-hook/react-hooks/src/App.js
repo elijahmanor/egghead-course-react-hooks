1da1a13d4942516370548c0f810bde1f429173fb
Elijah Manor <elijah.manor@gmail.com>
2018-11-24 08:23:03 -0600

    Get things back to a beginning state

--- 02-add-state-to-a-react-function-component-with-the-useState-hook/react-hooks/src/App.js
+++ 02-add-state-to-a-react-function-component-with-the-useState-hook/react-hooks/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
-import TodoList from "./TodoList.func";
+import Playground from "./Playground";
+// import TodoList from "./TodoList";
 
-export default () => <TodoList />;
+export default () => <Playground />;

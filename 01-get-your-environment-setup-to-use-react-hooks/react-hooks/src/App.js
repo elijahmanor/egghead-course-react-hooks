392385e96ea103354f0cfa79c7a78a412fbfa0bf
Elijah Manor <elijah.manor@gmail.com>
2018-11-24 07:46:42 -0600

    Remove the version piece from App.js

--- 01-get-your-environment-setup-to-use-react-hooks/react-hooks/src/App.js
+++ 01-get-your-environment-setup-to-use-react-hooks/react-hooks/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React v{React.version}
+            Learn React
           </a>
         </header>
       </div>

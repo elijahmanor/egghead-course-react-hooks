b7aeab1855a6c3517e9fd4d3a47b61b02fbb8167
Elijah Manor <elijah.manor@gmail.com>
2018-11-26 06:38:53 -0600

    updated 5 alpha func to 4 omega func

--- 05-use-the-useRef-hook-to-reference-a-dom-element/react-hooks/src/TodoList.func.js
+++ 05-use-the-useRef-hook-to-reference-a-dom-element/react-hooks/src/TodoList.func.js
@@ -1,61 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
-import styled from "react-emotion";
+import React, { useState, useEffect } from "react";
 import NewTodo from "./NewTodo";
 import TodoItem from "./TodoItem";
-import { Container, List } from "./Styled";
 import About from "./About";
-
-const styles = {
-  input: {
-    border: "2px solid rgba(255, 255, 255, 0.5)",
-    fontSize: "1.75em",
-    padding: "0.25em 0.5em",
-    color: "white",
-    borderRadius: "0.25em",
-    background: "transparent",
-    transition: "all 0.1s"
-  },
-  button: {
-    fontWeight: "400",
-    color: "black",
-    fontSize: "1.75em",
-    padding: "0.25em 0.5em",
-    backgroundColor: "rgba(255, 255, 255, 0.5)",
-    borderRadius: "0.25em",
-    margin: "5px",
-    cursor: "pointer"
-  }
-};
-
-export function Playground() {
-  const inputRef = useRef();
-  const handleClick = () => {
-    inputRef.current.focus();
-    inputRef.current.select();
-  };
-  return (
-    <section>
-      <input autoFocus ref={inputRef} style={styles.input} />
-      <button onClick={handleClick} style={styles.button}>
-        Focus
-      </button>
-    </section>
-  );
-}
+import { Container, List } from "./Styled";
 
 export default function TodoList() {
   const [newTodo, updateNewTodo] = useState("");
-  const todoId = useRef(0);
-  const initialTodos = () => {
-    const valueFromStorage = JSON.parse(
-      window.localStorage.getItem("todos") || "[]"
-    );
-    todoId.current = valueFromStorage.reduce(
-      (memo, todo) => Math.max(memo, todo.id),
-      0
-    );
-    return valueFromStorage;
-  };
+  const initialTodos = () =>
+    JSON.parse(window.localStorage.getItem("todos") || "[]");
   const [todos, updateTodos] = useState(initialTodos);
   useEffect(
     () => {
@@ -82,11 +34,10 @@ export default function TodoList() {
   }, []);
   const handleNewSubmit = e => {
     e.preventDefault();
-    todoId.current += 1;
     updateTodos(prevTodos => [
       ...prevTodos,
       {
-        id: todoId.current,
+        id: Date.now(),
         text: newTodo,
         completed: false
       }

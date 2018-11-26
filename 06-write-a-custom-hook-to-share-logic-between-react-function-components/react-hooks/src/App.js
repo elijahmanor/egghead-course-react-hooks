88f50fd776e6ad0381e630ee527014eec0e96e56
Elijah Manor <elijah.manor@gmail.com>
2018-11-26 07:50:25 -0600

    get lesson 6 alpha settled

--- 06-write-a-custom-hook-to-share-logic-between-react-function-components/react-hooks/src/App.js
+++ 06-write-a-custom-hook-to-share-logic-between-react-function-components/react-hooks/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
 import Playground from "./Playground.func";
-import TodoList from "./TodoList.class";
+import TodoList from "./TodoList.func";
 
 export default () => <TodoList />;

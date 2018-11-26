5566ca60c638bc516231ba6855fa8b6ec7a19d1f
Elijah Manor <elijah.manor@gmail.com>
2018-11-26 07:45:01 -0600

    Add some files for the lesson 6 alpha

---
+++ 06-write-a-custom-hook-to-share-logic-between-react-function-components/react-hooks/src/Playground.class.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Battery from "./Battery";
+
+export default class Playground extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = { level: 0, charging: false };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    navigator.getBattery().then(bat => {
+      this.battery = bat;
+      this.battery.addEventListener("levelchange", this.handleChange);
+      this.battery.addEventListener("chargingchange", this.handleChange);
+      this.handleChange({ target: this.battery });
+    });
+  }
+  componentWillUnmount() {
+    this.battery.removeEventListener("levelchange", this.handleChange);
+    this.battery.removeEventListener("chargingchange", this.handleChange);
+  }
+  handleChange({ target: { level, charging } }) {
+    this.setState({ level, charging });
+  }
+  render() {
+    return (
+      <section>
+        <Battery {...this.state} />
+      </section>
+    );
+  }
+}

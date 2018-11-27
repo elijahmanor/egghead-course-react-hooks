ac7bf0b46bc43a390f7c7d78ad30c2079d84d088
Elijah Manor <elijah.manor@gmail.com>
2018-11-27 06:24:20 -0600

    Add the hooks version of todo for lesosn 8

--- 08-use-the-useReducer-hook-and-dispatch-actions-to-update-state/react-hooks/src/TodoList.npm.js
+++ 08-use-the-useReducer-hook-and-dispatch-actions-to-update-state/react-hooks/src/TodoList.npm.js
@@ -1,1 +1,73 @@
 // placholder
+import React, { useState, useRef } from "react";
+import NewTodo from "./NewTodo";
+import TodoItem from "./TodoItem";
+import { Container, List } from "./Styled";
+import About from "./About";
+import { useLocalStorage, useKeyDown } from "./hooks";
+import { useTitle as useDocumentTitle } from "react-use";
+
+const incompleteTodoCount = todos =>
+  todos.reduce((memo, todo) => (!todo.completed ? memo + 1 : memo), 0);
+
+export default function TodoList() {
+  const [newTodo, updateNewTodo] = useState("");
+  const todoId = useRef(0);
+  const [todos, updateTodos] = useLocalStorage("todos", [], values => {
+    todoId.current = values.reduce((memo, item) => Math.max(memo, item.id), 0);
+  });
+  const inCompleteCount = incompleteTodoCount(todos);
+  const title = inCompleteCount ? `Todos (${inCompleteCount})` : "Todos";
+  useDocumentTitle(title);
+  let [showAbout, setShowAbout] = useKeyDown(
+    { "?": true, Escape: false },
+    false
+  );
+  const handleNewSubmit = e => {
+    e.preventDefault();
+    todoId.current += 1;
+    updateTodos(prevTodos => [
+      ...prevTodos,
+      {
+        id: todoId.current,
+        text: newTodo,
+        completed: false
+      }
+    ]);
+    updateNewTodo("");
+  };
+  const handleNewChange = e => updateNewTodo(e.target.value);
+  const handleDelete = (id, e) => {
+    updateTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+  };
+  const handleCompletedToggle = (id, e) => {
+    updateTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <Container todos={todos}>
+      <NewTodo
+        onSubmit={handleNewSubmit}
+        value={newTodo}
+        onChange={handleNewChange}
+      />
+      {!!todos.length && (
+        <List>
+          {todos.map(todo => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onChange={handleCompletedToggle}
+              onDelete={handleDelete}
+            />
+          ))}
+        </List>
+      )}
+      <About isOpen={showAbout} onClose={() => setShowAbout(false)} />
+    </Container>
+  );
+}

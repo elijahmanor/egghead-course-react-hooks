28d43a94e7d7df268f1c5f19915e7326cb0773aa
Elijah Manor <elijah.manor@gmail.com>
2018-11-26 18:47:48 -0600

    add todolist hook to alpha 7

--- 07-leverage-an-existing-community-hook-in-your-react-function-component/react-hooks/src/TodoList.hook.js
+++ 07-leverage-an-existing-community-hook-in-your-react-function-component/react-hooks/src/TodoList.hook.js
@@ -1,1 +1,113 @@
-// placeholder
+import React, { useState, useEffect, useRef } from "react";
+import NewTodo from "./NewTodo";
+import TodoItem from "./TodoItem";
+import { Container, List } from "./Styled";
+import About from "./About";
+
+const useLocalStorage = (key, defaultValue, callback) => {
+  const initialValue = () => {
+    const valueFromStorage = JSON.parse(
+      window.localStorage.getItem(key) || JSON.stringify(defaultValue)
+    );
+    if (callback) {
+      callback(valueFromStorage);
+    }
+    return valueFromStorage;
+  };
+  const [storage, updateStorage] = useState(initialValue);
+  useEffect(
+    () => {
+      window.localStorage.setItem(key, JSON.stringify(storage));
+    },
+    [storage]
+  );
+  return [storage, updateStorage];
+};
+
+const useDocumentTitle = title => {
+  useEffect(
+    () => {
+      document.title = title;
+    },
+    [title]
+  );
+};
+
+const useKeyDown = (map, defaultValue) => {
+  let [match, setMatch] = useState(defaultValue);
+  useEffect(() => {
+    const handleKey = ({ key }) => {
+      setMatch(prevMatch =>
+        Object.keys(map).some(k => k === key) ? map[key] : prevMatch
+      );
+    };
+    document.addEventListener("keydown", handleKey);
+    return () => document.removeEventListener("keydown", handleKey);
+  }, []);
+  return [match, setMatch];
+};
+
+const incompleteTodoCount = todos =>
+  todos.reduce((memo, todo) => (!todo.completed ? memo + 1 : memo), 0);
+
+export default function TodoList() {
+  const [newTodo, updateNewTodo] = useState("");
+  const todoId = useRef(0);
+  const [todos, updateTodos] = useLocalStorage("todos", [], values => {
+    todoId.current = values.reduce((memo, item) => Math.max(memo, item.id), 0);
+  });
+  const inCompleteCount = incompleteTodoCount(todos);
+  const title = inCompleteCount ? `Todos (${inCompleteCount})` : "Todos";
+  useDocumentTitle(title);
+  let [showAbout, setShowAbout] = useKeyDown(
+    { "?": true, Escape: false },
+    false
+  );
+  const handleNewSubmit = e => {
+    e.preventDefault();
+    todoId.current += 1;
+    updateTodos(prevTodos => [
+      ...prevTodos,
+      {
+        id: todoId.current,
+        text: newTodo,
+        completed: false
+      }
+    ]);
+    updateNewTodo("");
+  };
+  const handleNewChange = e => updateNewTodo(e.target.value);
+  const handleDelete = (id, e) => {
+    updateTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+  };
+  const handleCompletedToggle = (id, e) => {
+    updateTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <Container todos={todos}>
+      <NewTodo
+        onSubmit={handleNewSubmit}
+        value={newTodo}
+        onChange={handleNewChange}
+      />
+      {!!todos.length && (
+        <List>
+          {todos.map(todo => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onChange={handleCompletedToggle}
+              onDelete={handleDelete}
+            />
+          ))}
+        </List>
+      )}
+      <About isOpen={showAbout} onClose={() => setShowAbout(false)} />
+    </Container>
+  );
+}

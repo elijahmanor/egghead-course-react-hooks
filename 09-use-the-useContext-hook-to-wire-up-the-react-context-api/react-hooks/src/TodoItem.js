804620e808bdaea1660fb13489038e1831a558bf
Elijah Manor <elijah.manor@gmail.com>
2018-11-27 12:49:51 -0600

    finished up lesson 09 omega

--- 09-use-the-useContext-hook-to-wire-up-the-react-context-api/react-hooks/src/TodoItem.js
+++ 09-use-the-useContext-hook-to-wire-up-the-react-context-api/react-hooks/src/TodoItem.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import styled from "react-emotion";
 import Checkbox from "./Checkbox";
 import ThemeContext from "./ThemeContext";

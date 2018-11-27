e8f22cb7b0265d6992a8335e7894b785f43bb78e
Elijah Manor <elijah.manor@gmail.com>
2018-11-26 18:47:07 -0600

    add playground hook to alpha 7

--- 07-leverage-an-existing-community-hook-in-your-react-function-component/react-hooks/src/Playground.hook.js
+++ 07-leverage-an-existing-community-hook-in-your-react-function-component/react-hooks/src/Playground.hook.js
@@ -1,1 +1,35 @@
-// placeholder
+import React, { useState, useEffect } from "react";
+import Battery from "./Battery";
+
+const useBattery = () => {
+  const [battery, setBattery] = useState({ level: 0, charging: false });
+  const handleChange = ({ target: { level, charging } }) =>
+    setBattery({
+      level,
+      charging
+    });
+
+  useEffect(() => {
+    let battery;
+    navigator.getBattery().then(bat => {
+      battery = bat;
+      battery.addEventListener("levelchange", handleChange);
+      battery.addEventListener("chargingchange", handleChange);
+      handleChange({ target: battery });
+    });
+    return () => {
+      battery.removeEventListener("levelchange", handleChange);
+      battery.removeEventListener("chargingchange", handleChange);
+    };
+  }, []);
+  return battery;
+};
+
+export default function Playground() {
+  const battery = useBattery();
+  return (
+    <section>
+      <Battery {...battery} />
+    </section>
+  );
+}

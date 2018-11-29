96bd90a74e14ea0319fd9708bd9a15779e5aba27
Elijah Manor <elijah.manor@gmail.com>
2018-11-28 22:44:51 -0600

    Added class based component for playground in lesson 1 alpha

--- 02-add-state-to-a-react-function-component-with-the-useState-hook/react-hooks/src/Playground.js
+++ 02-add-state-to-a-react-function-component-with-the-useState-hook/react-hooks/src/Playground.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Playground() {
-  return (
-    <section>
-      <input type="text" value={""} onChange={e => {}} />
-      <input type="checkbox" checked={false} onChange={e => {}} />
-      <ul>
-        <li>{""}</li>
-        <li>{""}</li>
-      </ul>
-    </section>
-  );
+export default class Playground extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = { text: "", checked: false };
+  }
+  render() {
+    const { text, checked } = this.state;
+    return (
+      <section>
+        <input
+          type="text"
+          value={text}
+          onChange={e => this.setState({ text: e.target.value })}
+        />
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={e => this.setState({ checked: e.target.checked })}
+        />
+        <ul>
+          <li>{text}</li>
+          <li>{checked.toString()}</li>
+        </ul>
+      </section>
+    );
+  }
 }

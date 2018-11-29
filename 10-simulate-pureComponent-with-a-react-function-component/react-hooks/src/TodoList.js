ea85866a9e8af40967473fd493dd58303a7242cd
Elijah Manor <elijah.manor@gmail.com>
2018-11-29 16:22:07 -0600

    fix theme in lesson 10 alpha

--- 10-simulate-pureComponent-with-a-react-function-component/react-hooks/src/TodoList.js
+++ 10-simulate-pureComponent-with-a-react-function-component/react-hooks/src/TodoList.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import NewTodo from "./NewTodo";
 import TodoItem from "./TodoItem";
 import { Container, List } from "./Styled";
 import About from "./About";
 import { useTodosWithLocalStorage, useKeyDown } from "./hooks";
 import { useTitle as useDocumentTitle } from "react-use";
+import ThemeContext from "./ThemeContext";
 
 const incompleteTodoCount = todos =>
   todos.reduce((memo, todo) => (!todo.completed ? memo + 1 : memo), 0);
@@ -24,6 +25,7 @@ export default function TodoList() {
     dispatch({ type: "ADD_TODO", text: newTodo });
     updateNewTodo("");
   };
+  const theme = useContext(ThemeContext);
 
   return (
     <Container todos={todos}>
@@ -33,7 +35,7 @@ export default function TodoList() {
         onChange={e => updateNewTodo(e.target.value)}
       />
       {!!todos.length && (
-        <List theme="dark">
+        <List theme={theme}>
           {todos.map(todo => (
             <TodoItem
               key={todo.id}
